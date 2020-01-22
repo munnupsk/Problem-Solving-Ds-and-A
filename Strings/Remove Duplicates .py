9f077bcddb333854ed4544a4b37be6a1b752fca6
munnupsk <44919781+munnupsk@users.noreply.github.com>
2020-01-22 12:43:25 +0530

    Create Remove Duplicates .py

---
+++ Strings/Remove Duplicates .py
@@ -0,0 +1,46 @@
+
+
+#https://practice.geeksforgeeks.org/problems/remove-duplicates/0
+
+'''Example:
+   Input:
+   2
+  geeksforgeeks
+  geeks for geeks
+
+  Output:
+  geksfor
+  geks for '''
+
+
+
+
+
+
+
+
+def ls(s):
+    li=[]
+    for i in s:
+        li.append(i)
+    return li        
+for _ in range(int(input())):
+    s=input()
+    mutable_string=ls(s)#since the string is immutable
+    
+    bin_hash=[0]*256
+    temp=''
+    i_ind=0
+    j_ind=0
+    while i_ind!=len(mutable_string):
+        temp=mutable_string[i_ind]
+        if bin_hash[ord(temp)]==0:
+            bin_hash[ord(temp)]=1
+            mutable_string[j_ind]=temp
+            j_ind+=1
+        i_ind+=1
+    
+    print(''.join(mutable_string[0:j_ind]))
+   
+    
+
