abedf1a3ea44fe4803b51dcf82c555edaa54c3d8
Prasanna Kumar <prasannakumarpsk5555@gmail.com>
2020-04-29 10:47:42 +0530

    Create count leaves.py

---
+++ Tree/count leaves.py
@@ -0,0 +1,11 @@
+
+
+def countLeaves(root):
+    # Code here
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        return 1
+    
+    return countLeaves(root.left)+countLeaves(root.right)
+
